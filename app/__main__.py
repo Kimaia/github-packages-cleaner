bc447d50897cf57c72beb3c7c6d92f5118c2b0ff
Ivan Kolesnikov <ivan@kimaia.com>
2019-12-10 15:08:39 +0200

    Added --delete-base parameter.

--- app/__main__.py
+++ app/__main__.py
@@ -12,10 +12,11 @@ out_done = lambda m: click.secho(str(m), fg='green')
 @click.option("--owner", prompt="Repository owner", help="The owner of the repository.")
 @click.option("--name", prompt="Repository name", help="The person to greet.")
 @click.option("--all", is_flag=True, help="Remove all packages. Otherwise keep last version of each package.")
-def clean(token: str, owner: str, name: str, all: bool):
+@click.option("--delete-base", is_flag=True, help="Remove also docker base layer(Dangerous!).")
+def clean(token: str, owner: str, name: str, all: bool, delete_base: bool):
     """Removes all packages except current versions from Github."""
 
-    cleanup(token, owner, name, all, out_info=out_info, out_action=out_action, out_done=out_done)
+    cleanup(token, owner, name, all, delete_base, out_info=out_info, out_action=out_action, out_done=out_done)
 
 
 if __name__ == '__main__':
--- app/cleanup.py
+++ app/cleanup.py
@@ -1,14 +1,19 @@
-from .packages_list import packages_list
 from .packages_delete import packages_delete
+from .packages_list import packages_list
+
+DOCKER_BASE_LAYER_NAME = 'docker-base-layer'
 
 
-def cleanup(token: str, name: str, owner: str, all: bool, out_info=print, out_action=print, out_done=print):
+def cleanup(token: str, name: str, owner: str, all: bool, delete_base: bool, out_info=print, out_action=print, out_done=print):
     packages = packages_list(token, name, owner)
 
     for p in packages:
         out_info(f"Cleaning package {p.name}...")
         for v in p.versions:
             if v.id == p.last_version_id and not all:
+                continue
+
+            if not delete_base and v.version == DOCKER_BASE_LAYER_NAME:
                 continue
 
             out_action(f"    Removing package version {v.version}...")
